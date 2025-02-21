39866753ea34f6d88d61111469f57d92144b0444
shunsei <k.shunsei1227@gmail.com>
2025-02-21 14:49:53 +0900

    Send notification to Slack when excpetion

--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+from slack_sdk import WebhookClient
+
 from reader import FeliCaReader
 
 
 if __name__ == '__main__':
-    reader = FeliCaReader()
-    reader.read()
+    try:
+        reader = FeliCaReader()
+        reader.read()
+    except Exception as e:
+        load_dotenv()
+        SLACK_WEBHOOK_URL = os.getenv('SLACK_WEBHOOK_URL')
+
+        client = WebhookClient(SLACK_WEBHOOK_URL)
+        response = client.send(
+            text="スマートロックが停止しました",
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"```{e}```"
+                    }
+                }
+            ]
+        )
