a84fd1bbb3083b699a555374b2411f645d9b1bd4
shunsei <k.shunsei1227@gmail.com>
2025-03-14 15:34:42 +0900

    Hardcoding member id

--- src/auth.py
+++ src/auth.py
@@ -1,14 +1,27 @@
 def check_student_id(student_id: str) -> bool:
     if not student_id.isdigit():
         return False
-    
-    student_id1 = int(student_id)
 
-    with open('student_id.txt', 'r') as file:
-        for line in file:
-            student_id2 = int(line.strip())
-            if student_id1 == student_id2:
-                return True
+    member_id_list = [
+        # B4
+        '21122015',
+        '21122017',
+        '21122020',
+        '21122038',
+        # M1
+        '24622017',
+        '24622033',
+        '24622044',
+        # M2
+        '23622010',
+        '23622026',
+        '23622047',
+        # D2
+        '23821004',
+    ]
+
+    if student_id in member_id_list:
+        return True
 
     return False
 
